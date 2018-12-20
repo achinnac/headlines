685c4b4a3684427553eb7351e65401d5bb83f671
Arthit Chinnachot <achinnac@gmail.com>
2018-12-19 19:23:43 -0800

    added request and form

--- headlines.py
+++ headlines.py
@@ -1,5 +1,7 @@
 import feedparser
 from flask import Flask, render_template
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -8,11 +10,18 @@ app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+             'iol': 'http://www.iol.co.za/cmlink/1.640',
+             'rt': 'http://feeds.reuters.com/reuters/technologyNews',
+             'abc': 'https://abcnews.go.com/abcnews/topstories'}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
+    
     feed = feedparser.parse(RSS_FEEDS[publication])
     return render_template("home.html", articles=feed['entries'])
 
